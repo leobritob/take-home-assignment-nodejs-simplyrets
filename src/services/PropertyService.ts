c4d2dec2deb1c8b4b8bcea85a96188472e7cbef2
Leonardo Brito Bittencourt <leonardobritobittencourt@gmail.com>
2023-07-23 10:36:18 -0300

    feat(property): creating service and its unit test

--- src/services/PropertyService.ts
+++ src/services/PropertyService.ts
@@ -1,1 +1,32 @@
-export class PropertyService {}
+import AppDataSource from '../dataSource';
+import { Property } from '../entities';
+
+export class PropertyService {
+  constructor(private readonly propertyRepository = AppDataSource.getRepository(Property)) {}
+
+  async findAll() {
+    return this.propertyRepository.find();
+  }
+
+  async createOne(data: any) {
+    return this.propertyRepository.save(this.propertyRepository.create(data));
+  }
+
+  async findOneByIdOrThrow(id: number) {
+    const property = await this.propertyRepository.findOne({ where: { id } });
+    if (!property) {
+      throw new Error('Property not found');
+    }
+    return property;
+  }
+
+  async updateOneById(id: number, data: any) {
+    const property = await this.findOneByIdOrThrow(id);
+    this.propertyRepository.merge(property, data);
+    return this.propertyRepository.save(property);
+  }
+
+  async deleteOneById(id: number) {
+    await this.propertyRepository.delete(id);
+  }
+}
---
+++ src/services/__tests__/PropertyService.spec.ts
@@ -0,0 +1,75 @@
+import AppDataSource, { seedDb } from '../../dataSource';
+import { PropertyService } from '../PropertyService';
+
+describe('PropertyService', () => {
+  let propertyService: PropertyService;
+
+  beforeAll(async () => {
+    await AppDataSource.initialize();
+    await seedDb();
+  });
+
+  beforeEach(() => {
+    propertyService = new PropertyService();
+  });
+
+  it('should be able to get all properties', async () => {
+    // Act
+    const list = await propertyService.findAll();
+    // Asert
+    expect(list).toBeDefined();
+    expect(list.length).toBeGreaterThanOrEqual(1);
+  });
+
+  it('should be able to create a new property', async () => {
+    // Arrange
+    const data = {
+      id: 127,
+      address: '963 Blackwell Street #10',
+      price: 850200,
+      bedrooms: 1,
+      bathrooms: 1,
+      type: 'Townhouse',
+    };
+    // Act
+    const item = await propertyService.createOne(data);
+    // Asert
+    expect(item).toBeDefined();
+  });
+
+  it('should be able to get one property finding by id', async () => {
+    // Arrange
+    const id = 1;
+    // Act
+    const item = await propertyService.findOneByIdOrThrow(id);
+    // Asert
+    expect(item).toBeDefined();
+  });
+
+  it('should be able to throw an error when try to get one property finding by id', async () => {
+    // Arrange
+    const id = 1e9;
+    // Asert
+    expect(propertyService.findOneByIdOrThrow(id)).rejects.toThrowError(new Error('Property not found'));
+  });
+
+  it('should be able to update one property finding by id', async () => {
+    // Arrange
+    const id = 1;
+    const data = { bedrooms: 3 };
+    // Act
+    const item = await propertyService.updateOneById(id, data);
+    // Asert
+    expect(item).toBeDefined();
+    expect(item.bedrooms).toEqual(data.bedrooms);
+  });
+
+  it('should be able to delete one property finding by id', async () => {
+    // Arrange
+    const id = 1;
+    // Act
+    await propertyService.deleteOneById(id);
+    // Asert
+    expect(propertyService.findOneByIdOrThrow(id)).rejects.toThrowError(new Error('Property not found'));
+  });
+});
